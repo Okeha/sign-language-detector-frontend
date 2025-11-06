bc9bc286f1e5b769caa2ac5d8aa2851ab2e3ecbe
Anthony OKEH <anthony.okeh@alten.co.uk>
2025-11-06 17:17:16 +0000

    Style: Added Side Panel

--- personal/signLanguage/frontend/sign-language-detector/app/layout.tsx
+++ personal/signLanguage/frontend/sign-language-detector/app/layout.tsx
@@ -25,9 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <HomeLayout>{children}</HomeLayout>
         {/* <h1>Hello, World!</h1> */}
       </body>
